e3f7b41c31e5ab373691065be437a58a42b961bc
Ewomazino Ek <ewomazinoek@gmail.com>
2025-04-10 09:08:20 +0100

    Updated Database Connection

--- src/main/java/com/propertyrental/config/DBConnection.java
+++ src/main/java/com/propertyrental/config/DBConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "postgresql://postgres:[X6LSjw7Ew5FxUdGN]@db.fjnuogmmopfcriwbaben.supabase.co:5432/postgres";
-    private static final String USER = "postgres"; // Change if needed
-    private static final String PASSWORD = "admin"; // Replace with your actual password
+    private static final String URL = "jdbc:postgresql://onlineprs_database_user:pWa5iO5tZCTVCFxqwXrTuoEWvIfQl8tP@dpg-cvrnfo6r433s73avjojg-a/onlineprs_database";
+    private static final String USER = "onlineprs_database_user"; // Change if needed
+    private static final String PASSWORD = "pWa5iO5tZCTVCFxqwXrTuoEWvIfQl8tP"; // Replace with your actual password
 
     public static Connection getConnection() {
         try {
