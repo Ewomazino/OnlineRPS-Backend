500f545d7169fb9e7c97a7c8c595892d772bfac4
Ewomazino Ek <ewomazinoek@gmail.com>
2025-04-10 09:38:19 +0100

    UPDATED SQL CONNECTION

--- src/main/java/com/propertyrental/config/DBConnection.java
+++ src/main/java/com/propertyrental/config/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "jdbc:postgresql://onlineprs_database_user:pWa5iO5tZCTVCFxqwXrTuoEWvIfQl8tP@dpg-cvrnfo6r433s73avjojg-a/onlineprs_database";
+    private static final String URL = "jdbc:postgresql://onlineprs_database_user:pWa5iO5tZCTVCFxqwXrTuoEWvIfQl8tP@dpg-cvrnfo6r433s73avjojg-a.oregon-postgres.render.com/onlineprs_database";
     private static final String USER = "onlineprs_database_user"; // Change if needed
     private static final String PASSWORD = "pWa5iO5tZCTVCFxqwXrTuoEWvIfQl8tP"; // Replace with your actual password
 
