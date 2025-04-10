71fe2a5a190c65e65fde6ecf3c7be3da93588f6a
Ewomazino Ek <ewomazinoek@gmail.com>
2025-04-10 09:57:48 +0100

    Database connection

--- src/main/java/com/propertyrental/config/DBConnection.java
+++ src/main/java/com/propertyrental/config/DBConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String URL = "postgresql://onlineprs_database_user:pWa5iO5tZCTVCFxqwXrTuoEWvIfQl8tP@dpg-cvrnfo6r433s73avjojg-a/onlineprs_database";
+    private static final String URL = "jdbc:postgresql://dpg-cvrnfo6r433s73avjojg-a:5432/onlineprs_database?user=onlineprs_database_user&password=pWa5iO5tZCTVCFxqwXrTuoEWvIfQl8tP&sslmode=require";
     private static final String USER = "onlineprs_database_user"; // Change if needed
     private static final String PASSWORD = "pWa5iO5tZCTVCFxqwXrTuoEWvIfQl8tP"; // Replace with your actual password
 
